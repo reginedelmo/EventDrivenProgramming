b0b3ef226fc464604e28da22a56896f9fb725a4d
Regine <68411973+sayitsjen@users.noreply.github.com>
2022-02-09 14:22:32 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-function minusFunc(){
-    var value = document.getElementById("number").innerHTML;
+function subtotalFunc(val){
+    document.getElementById("subtotal").innerHTML = val;
 
-    --value;
+    let value = val;
+    if(value < 100 || value > 10000){
+        console.log("Input invalid. Value = " + value);
+    }
+    else {
+        const tax = 0.12;
+        let valueTax = val*tax;
+        let total = (valueTax + parseInt(val));
 
-    if(value < 0 && value > -10){
-        var sign = String(value).charAt(0);
-        var num = String(value).charAt(1);
-        value = sign + "0" + num;
+        document.getElementById("tax").value = valueTax.toFixed(2);
+        document.getElementById("total").value = total.toFixed(2);
+        document.getElementById("payment").value = total.toFixed(2);
+        document.getElementById("payment").disabled = false;
+
+        const amount = document.getElementById('payment').value;
+        payment.addEventListener('change', (e) => { 
+            let newAmount = (e.target.value);
+            let amountChange = (newAmount - total);
+
+            document.getElementById("change").value = amountChange.toFixed(2);
+
+            if(amountChange < 0){
+                document.getElementById("btnpay").style.background='#631111';
+                document.getElementById("btnpay").onclick = false;
+            }
+            else{
+                document.getElementById("btnpay").style.background='#0A460C';
+                document.getElementById("btnpay").onclick = function() { payFunction(); }
+            }
+          });
+
+        let change = (amount - total);
+        
+        document.getElementById("change").value = change.toFixed(2);
+        document.getElementById("btnpay").style.background='#0A460C';
+        document.getElementById("btnpay").onclick = function() { payFunction(); }
     }
-    if (value >= 0 && value < 10){
-        value = "0" + value;
-    }
-    document.getElementById("number").innerHTML = value;
 }
 
-function addFunc(){
-    var value = document.getElementById("number").innerHTML;
-
-    ++value;
-
-    if(value < 0 && value > -10){
-        var sign = String(value).charAt(0);
-        var num = String(value).charAt(1);
-        value = sign + "0" + num;
-    }
-    if (value >= 0 && value < 10){
-        value = "0" + value;
-    }
-    document.getElementById("number").innerHTML = value;
-}
-
-function copyFunc(){
-    document.getElementById("itext").value = document.getElementById("number").innerHTML;
+function payFunction() {
+    alert("Pay");
 }
